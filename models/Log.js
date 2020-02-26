ad06778a806b77d45a1b0351969815879beda284
Perdikal <Alexis.perd@yahoo.com>
2020-02-26 12:52:33 +0100

    first push

---
+++ models/Log.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const LogSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    comment: String,
+    Project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project"
+    }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Log = mongoose.model("Log", LogSchema);
+module.exports = Log;
