450b0f57c316bbe1b7051fd65061a0b524ecbef8
ciwan_aram <aram.juan@gmail.com>
2020-02-26 12:39:08 +0100

    Task Model

---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
