fc5533444785a05f97a39bf75fd94f4eff258150
ciwan_aram <aram.juan@gmail.com>
2020-02-26 16:52:11 +0100

    checking before merge

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const usersRoutes = require("./users");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.send('Hello');
 });
 
 module.exports = router;
