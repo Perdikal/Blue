9a0a75fd620078ecdd5cba3a79e58f5e21f65c75
Perdikal <Alexis.perd@yahoo.com>
2020-02-27 16:09:32 +0100

    project routes done

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const Project = require("../models/Project");
 const User = require("../models/User");
+const Task = require("../models/Task");
+const Log = require("../models/Log");
 
 const loginCheck = (req, res, next) => {
   if (req.user) {
@@ -9,11 +11,69 @@ const loginCheck = (req, res, next) => {
     res.redirect("/");
   }
 };
+
+router.post("/project/:id/changestatus/:taskid", loginCheck, (req, res) => {
+  const projectId = req.params.id;
+  const ticketId = req.params.taskid;
+  const { status } = req.body;
+  Task.findById(ticketId).then(ticket => {
+    ticket.update({ status: status }).then(task => {
+      res.json(task);
+    });
+  });
+});
+
+router.get("/project/:id/log", loginCheck, (req, res) => {
+  const projectId = req.params.id;
+  Log.find({ project: projectId })
+    .then(logs => {
+      res.json(logs);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.post("/project/:id/log", loginCheck, (req, res) => {
+  const projectId = req.params.id;
+  const { comment } = req.body;
+  Log.create({
+    author: req.user._id,
+    comment: comment,
+    project: projectId
+  })
+    .then(log => {
+      res.json(log);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.post("/project/:id/createtask", loginCheck, (req, res) => {
+  const projectId = req.params._id;
+  const id = req.user._id;
+  const { title, description, assignee } = req.body;
+  Task.create({
+    title: title,
+    description: description,
+    author: id,
+    assignee: assignee
+  }).then(task => {
+    Project.findByIdAndUpdate(projectId, { $push: { tasks: task._id } }).then(
+      () => {
+        res.json({ message: "Alles good" });
+      }
+    );
+
+    // res.json(task);
+  });
+});
+
 router.get("/project/:id", loginCheck, (req, res) => {
-  console.log("ijoeaijfoiea");
   const projectId = req.params.id;
   res.json({ responseKey: projectId });
-  /*  Project.findById(projectId)
+  Project.findById(projectId)
     .then(project => {
       res.json(project);
     })
@@ -21,7 +81,7 @@ router.get("/project/:id", loginCheck, (req, res) => {
       res.status(500).json({
         message: err.message
       });
-    }); */
+    });
 });
 
 router.get("/", (req, res) => {
