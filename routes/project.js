dc253d64a97c235ac6dba3be6c46742a2d2b7c6b
ciwan_aram <aram.juan@gmail.com>
2020-02-28 17:25:52 +0100

    pulling

--- routes/project.js
+++ routes/project.js
@@ -1,19 +1,18 @@
-const router = require("express").Router();
-const Project = require("../models/Project");
-const User = require("../models/User");
-const Task = require("../models/Task");
-const Log = require("../models/Log");
+const router = require('express').Router();
+const Project = require('../models/Project');
+const User = require('../models/User');
+const Task = require('../models/Task');
+const Log = require('../models/Log');
 
 const loginCheck = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
 };
 
-router.post("/project/:id/changestatus/:taskid", loginCheck, (req, res) => {
-  const projectId = req.params.id;
+router.post('/project/:id/changestatus/:taskid', loginCheck, (req, res) => {
   const ticketId = req.params.taskid;
   const { status } = req.body;
   Task.findById(ticketId).then(ticket => {
@@ -23,7 +22,7 @@ router.post("/project/:id/changestatus/:taskid", loginCheck, (req, res) => {
   });
 });
 
-router.get("/project/:id/log", loginCheck, (req, res) => {
+router.get('/project/:id/log', loginCheck, (req, res) => {
   const projectId = req.params.id;
   Log.find({ project: projectId })
     .then(logs => {
@@ -34,7 +33,7 @@ router.get("/project/:id/log", loginCheck, (req, res) => {
     });
 });
 
-router.post("/project/:id/log", loginCheck, (req, res) => {
+router.post('/project/:id/log', loginCheck, (req, res) => {
   const projectId = req.params.id;
   const { comment } = req.body;
   Log.create({
@@ -50,7 +49,7 @@ router.post("/project/:id/log", loginCheck, (req, res) => {
     });
 });
 
-router.get("/project/bringmine", loginCheck, (req, res) => {
+router.get('/project/bringmine', loginCheck, (req, res) => {
   Project.find({ members: req.user._id })
     .then(projectList => {
       res.json(projectList);
@@ -60,7 +59,7 @@ router.get("/project/bringmine", loginCheck, (req, res) => {
     });
 });
 
-router.post("/project/:id/createtask", loginCheck, (req, res) => {
+router.post('/project/:id/createtask', loginCheck, (req, res) => {
   const projectId = req.params._id;
   const id = req.user._id;
   const { title, description, assignee } = req.body;
@@ -72,20 +71,20 @@ router.post("/project/:id/createtask", loginCheck, (req, res) => {
   }).then(task => {
     Project.findByIdAndUpdate(projectId, { $push: { tasks: task._id } }).then(
       () => {
-        res.json({ message: "Alles good" });
+        res.json({ message: 'Alles good' });
       }
     );
   });
 });
 
-router.get("/project/:id/tasks", loginCheck, (req, res) => {
+router.get('/project/:id/tasks', loginCheck, (req, res) => {
   const projectId = req.params.id;
   Task.find({ project: projectId }).then(taskList => {
     res.json(taskList);
   });
 });
 
-router.get("/project/:id", loginCheck, (req, res) => {
+router.get('/project/:id', loginCheck, (req, res) => {
   const projectId = req.params.id;
   res.json({ responseKey: projectId });
   Project.findById(projectId)
@@ -97,10 +96,6 @@ router.get("/project/:id", loginCheck, (req, res) => {
         message: err.message
       });
     });
-});
-
-router.get("/", (req, res) => {
-  console.log("wooorks");
 });
 
 module.exports = router;
