c04dc537bdfe324b5cde3f127aa53f5ec37868c0
Lluís Mañas <manastorra@gmail.com>
2020-02-28 17:21:30 +0100

    home page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,29 @@ import Signup from "./components/signup/Signup";
 import Info from "./components/info/Info";
 import Steps from "./components/steps/Steps";
 import axios from "axios";
+import Footer from "./components/footer/Footer";
 
 class App extends React.Component {
   state = {
     user: this.props.user
   };
+
+  setUser = userObj => {
+    this.setState({
+      user: userObj
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar setUser={this.setUser} user={this.state.user} />
         <h2>Blue</h2>
+        <Signup setUser={this.setUser} user={this.state.user} />
 
         <Info />
         <Steps />
+        <Footer />
         {/* <info user= {}/> */}
       </div>
     );
