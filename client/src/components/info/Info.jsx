398a5b53439bff0e69e60f891ffaeaa1fe471277
Lluís Mañas <manastorra@gmail.com>
2020-02-28 12:07:19 +0100

    login, info and steps

---
+++ client/src/components/info/Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo
+        doloremque ad. Enim mollitia maxime voluptatem exercitationem officiis
+        ex distinctio, beatae aliquid fugiat pariatur, at obcaecati deserunt ab
+        rem nemo?
+      </p>
+    </div>
+  );
+};
+
+export default Info;
