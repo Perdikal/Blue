151ea322edcba0c773dc28c33c8ffdcae85fc2eb
ciwan_aram <aram.juan@gmail.com>
2020-03-03 18:05:00 +0100

    finding errors

--- client/src/components/tasksDashboard/TaskDashboard.jsx
+++ client/src/components/tasksDashboard/TaskDashboard.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import Task from "./task/Task";
-import Log from "./logComp/Log";
+import React, { Component } from 'react';
+import Task from './task/Task';
+import Log from './logComp/Log';
 
 export default class TaskDashboard extends Component {
   state = {
     tasks: [],
     showForm: false
   };
-  // componentDidMount() {
-  //   this.getProjectData();
-  // }
 
   updateAddedTasks = task => {
     console.log('Does this even work');
@@ -26,14 +22,10 @@ export default class TaskDashboard extends Component {
     return (
       <div>
         <Log />
-        <Task params={this.props.match.params} updateAddedTasks={this.updateAddedTasks} />
-        {/* <Route
-          exact
-          path="/project/:id"
-          render={props => (
-            <Task {...props} id={Boolean(this.state.user)} />
-          )}
-        /> */}
+        <Task
+          params={this.props.match.params}
+          updateAddedTasks={this.updateAddedTasks}
+        />
       </div>
     );
   }
