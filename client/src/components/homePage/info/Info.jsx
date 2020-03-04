f2e53ae46d7a0939ead41f020064dfa8e612647e
Perdikal <Alexis.perd@yahoo.com>
2020-03-04 07:47:55 +0100

    pulling everything

--- client/src/components/homePage/info/Info.jsx
+++ client/src/components/homePage/info/Info.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import Paragraph from "./Info";
 
 const Info = () => {
   return (
     <div>
-      <p>
+      <Paragraph>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo
         doloremque ad. Enim mollitia maxime voluptatem exercitationem officiis
         ex distinctio, beatae aliquid fugiat pariatur, at obcaecati deserunt ab
         rem nemo?
-      </p>
+      </Paragraph>
     </div>
   );
 };
