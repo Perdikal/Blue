1120b42f95d6ade9a3b6396b5d4518dcd8af241e
ciwan_aram <aram.juan@gmail.com>
2020-03-04 17:12:35 +0100

    no change

--- client/src/components/profilePage/ProfilePage.jsx
+++ client/src/components/profilePage/ProfilePage.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import UserInfo from "./userInfo/UserInfo";
-import Project from "./project/Project";
-import NewProjectForm from "./NewProjectForm/NewProjectForm";
+import React, { Component } from 'react';
+import UserInfo from './userInfo/UserInfo';
+import Project from './project/Project';
+import NewProjectForm from './NewProjectForm/NewProjectForm';
 //import Navbar from "../Navbar/Navbar";
 
 export default class ProfilePage extends Component {
   render() {
-    console.log("USER", this.props.user);
+    console.log('USER', this.props.user);
+    console.log('Project hahaha', this.props.project);
     return (
       <div>
         <UserInfo setUser={this.props.setUser} user={this.props.user} />
